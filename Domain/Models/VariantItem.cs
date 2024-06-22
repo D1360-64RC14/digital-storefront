0d74dd45f97a9ed6d14daf329542e42afae4368d
D1360-64RC14 <d1360_64rc14@protonmail.com>
2024-06-22 00:51:57 -0300

    feat: variant item

---
+++ Domain/Models/VariantItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models;
+
+public class VariantItem
+{
+    public required Guid Id { get; set; }
+    public required string Label { get; set; }
+}
